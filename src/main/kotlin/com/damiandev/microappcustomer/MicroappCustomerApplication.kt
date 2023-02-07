abaaaab4581c595bd4e8f80609c873414b6e6d48
damiandev <damian.kalicki@o2.pl>
2023-02-07 19:28:48 +0100

    Initial project commit

---
+++ src/main/kotlin/com/damiandev/microappcustomer/MicroappCustomerApplication.kt
@@ -0,0 +1,11 @@
+package com.damiandev.microappcustomer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MicroappCustomerApplication
+
+fun main(args: Array<String>) {
+	runApplication<MicroappCustomerApplication>(*args)
+}
---
+++ src/test/kotlin/com/damiandev/microappcustomer/MicroappCustomerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.damiandev.microappcustomer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MicroappCustomerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
